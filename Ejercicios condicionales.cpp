91e073ef1f458be864b9fffef27bc1205c2497a6
AbrahamPP2000 <97979648+AbrahamPP2000@users.noreply.github.com>
2022-09-06 19:43:53 -0500

    Update Ejercicios condicionales.cpp

--- Ejercicios condicionales.cpp
+++ Ejercicios condicionales.cpp
@@ -55,7 +55,7 @@ enum {ninguno = 0, ejercicio1, ejercicio2, ejercicio3, ejercicio4,
       ejercicio5, ejercicio6, ejercicio7, ejercicio8, ejercicio9,
       ejercicio10, ejercicio11, ejercicio12};
 
-void primerEjercicio()
+void primerEjercicio();
 void segundoEjercicio();
 void tercerEjercicio();
 void cuartoEjercicio();
